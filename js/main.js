5fe329fe58c32aa724552cc69cf8e954eabd50b6
Chase <chaseofchase@yahoo.com>
2013-06-25 20:21:15 -0700

    Revert "Password Protect"
    This reverts commit 2c958dcc19d6a71adb53ae3112ccce2634806041.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
- /*$("html").hide();
- 
+/*$(document).ready(function() {
  var pass = prompt("Please Enter Password");
-
+ 
+ $(html).hide();
+ 
  if(pass === "gittisarn"){
  	alert("Welcome");
- 	$("html").show();
+ 	$(html).show();
  }
  else{
  	alert("Wrong Password Please Refresh And Try Again");
+ 	$(html).hide();
  }
+});
 */
