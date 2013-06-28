830cc596e07e25f967443a7d33204550b4e27607
Chase <chaseofchase@yahoo.com>
2013-06-28 09:22:42 -0700

    Delete etra links

--- Nav.php
+++ Nav.php
@@ -1,17 +1,8 @@
-<!--<div class="nav">
-	<a href="index.php"><img src="Pictures/surfboard.jpg"></a>
-	<a href="Chase.php"><img src="Pictures/surfboard.jpg"></a>
-	<a href="London.php"><img src="Pictures/surfboard.jpg"></a>
-	<a href="Holland.php"><img src="Pictures/surfboard.jpg"></a>
-	<a href="Brooklyn.php"><img src="Pictures/surfboard.jpg"></a>
-	<a href="Rome.php"><img src="Pictures/surfboard.jpg"></a>
-</div>-->
-	
-<di class="nav">
+<div class="nav">
 	<div><a class="home" href="index.php">Home</a></div>
 	<div><a class="chase"href="Chase.php">Maverick</a></div>
 	<div><a class="london" href="London.php">Breeze</a></div>
 	<div><a class="holland" href="Holland.php">Rider</a></div>
 	<div><a class="brooklyn" href="Brooklyn.php">Kiwi</a></div>
 	<div><a class="rome" href="Rome.php">Rock</a></div>
-</di>
+</div>
