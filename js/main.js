43ebd6943b0bf1141247a79acb09fffda1be26ef
Chase <chaseofchase@yahoo.com>
2013-07-26 12:03:23 -0700

    Make search engine work

--- js/main.js
+++ js/main.js
@@ -1,83 +1,39 @@
-/*
- $("html").hide();
- 
-var pass = prompt("Please Enter Password");
 
- if(pass === "gittisarn"){
- 	$("html").show();
- }
- else{
- 	alert("Wrong Password Please Refresh And Try Again");
- }
-*/
-
-
-$("form").submit(function() {
-	return false;
+$(document).ready(function() {
+	
+	$("form").submit(function() {
+		return false;
+	});
+	
+ 	$(".submit").click(function(){
+	
+		var searchTerms = {
+			"home": "index.php",
+			"chase": "Chase.php",
+			"london": "London.php",
+			"holland": "Holland.php",
+			"brooklyn": "Brooklyn.php",
+			"rome": "Rome.php"
+		};
+		
+		var value = searchTerms[$("input").val().toLowerCase()];
+		
+		if(typeof(value) == "undefined"){
+			alert("Page Not Found");
+		}
+		else{
+			location.href = value;
+		}
+	});
 });
 
+	
+	
+	
+	
+	
+	
 
-$("button").click(function(){
-	
-	var searchTerms = {
-		"home": "index.php",
-		"chase": "Chase.php"
-	};
-	
-	var input = $("input").val();
-	
-	
-	console.log(searchTerms["home"]);
-	
-	
-	// switch (input) {
-		
-	// 	case "home":
-	// 		location.href = 'index.php';
-	// 	break;
-		
-	// 	case "chase":
-	// 		location.href = 'Chase.php';
-	// 	break;
-	// }
-	
-	
-	
-	
-	// if(input == "home"){
-	// 	location.href = 'index.php';
-	// }
-		
-	// if(input == "chase"){
-	// 		location.href = 'Chase.php';
-	// }
-		
-	// if(input == "london"){
-	// 		location.href = 'London.php';
-	// }
-		
-	// if(input == "holland"){
-	// 		location.href = 'Holland.php';
-	// }
-		
-	// if(input == "brooklyn"){
-	// 		location.href = 'Brooklyn.php';
-	// }
-		
-	// if(input == "rome"){
-	// 		location.href = 'Rome.php';
-	// }
-	
-});
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
