cbee7b24f67e8e74a34a8a704cb61da71725f26c
Chase James <chaseofchase@yahoo.com>
2013-08-20 11:21:08 -0700

    Attempted to make clean urls. Might do later.

--- inc/Head.php
+++ inc/Head.php
@@ -15,7 +15,7 @@
 		<div class="margin"></div>
 		
 		<nav>
-			<div><a class="<?php if($section == "home"){echo "on";} ?>" href="/index.php">Home</a></div>
+			<div><a class="<?php if($section == "home"){echo "on";} ?>" href="index.php">Home</a></div>
 			<div><a class="<?php if($section == "chase"){echo "on";} ?>" href="chase.php">Maverick</a></div>
 			<div><a class="<?php if($section == "london"){echo "on";} ?>" href="london.php">Breeze</a></div>
 			<div><a class="<?php if($section == "holland"){echo "on";} ?>" href="holland.php">Rider</a></div>
